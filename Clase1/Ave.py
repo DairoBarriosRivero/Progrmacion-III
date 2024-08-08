6fb72a805c87f95447a37ab43de687b7ef7de9e7
DairoBarriosRivero <160241214+DairoBarriosRivero@users.noreply.github.com>
2024-08-08 11:59:01 -0500

    Add files via upload

---
+++ Clase1/Ave.py
@@ -0,0 +1,23 @@
+from Animal import *
+
+class Ave(Animal):
+    def __init__(self, Nombre, Peso, Año_Nacimiento, Propietario):
+        super().__init__(Nombre, Peso)
+        self.Año_Nacimiento = Año_Nacimiento
+        self.Propietario = Propietario
+        
+        
+    def calcularedad(self):
+        edad = 2024-self.Año_Nacimiento
+        if edad >= 5:
+            print("Mayor de edad")
+        elif edad <4:
+            print("Menor de edad")     
+
+    def __str__(self):
+        return f"Ave: {self.Nombre}, Peso: {self.Peso} kg, Año de Nacimiento: {self.Año_Nacimiento}, Propietario: {self.Propietario}"   
+    
+ave1 = Ave("Paloma",3,2012,"Dairo")
+print(ave1)
+ave1.calcularedad()
+
